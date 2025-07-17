8db86db56aaff326ee6d4b93f3aaea6bebaef588
Yukimura-Hanzo <t.personal.business@gmail.com>
2025-07-17 18:56:22 +0100

    refactor: converted to plain css removed scss version

--- src/app/(ui)/footer.tsx
+++ src/app/(ui)/footer.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 const Footer = () => {
   //? Variable to store values for todays date
   // const currentDate = new Date();
   const currentYear = new Date().toLocaleDateString('en-US', { year: 'numeric' });
 
   return (
-    <footer className="footer mt-4">
+    <footer className="footer">
       <small>
         Copyright &copy; {currentYear} YUKIMURA HANZO
       </small>
